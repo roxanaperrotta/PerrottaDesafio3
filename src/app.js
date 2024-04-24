e2800601c82eaa38cc1604135c4c83b86c23a4da
roxanaperrotta <136701143+roxanaperrotta@users.noreply.github.com>
2024-04-24 16:00:37 -0300

    PreEntrega1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import ProductManager from './ProductManager.js';
+import productsRouter from "./routes/productsRouter.js";
+import cartsRouter from "./routes/cartsRouter.js";
 
 const app = express ();
 const PORT= 8080;
@@ -7,35 +9,13 @@ const PORT= 8080;
 const productManager =  new ProductManager ("./.products.json");
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/", productsRouter);
 
-app.get('/products',  async (req, res)=>{
-    const data=  await productManager.getProducts() 
-    
-    const limite = parseInt(req.query.limit);
-  
-    const products = limite ? data.slice(0,limite) : data
-   
-      res.json(products);
-   
-        
+app.use("/", cartsRouter);
 
-})
-
-
-
-
-app.get('/productId/:productId',  async (req, res)=>{
-    const productId = req.params.productId;
-    const data = await productManager.getProducts();
-   // const data = await productManager.getProductById(productId);
-    const product = data.find((p)=>p.id==productId);
-
-    if (!product) {
-        return res.status(404).json({ error: "Producto no encontrado" });
-      }
-       res.json(product);
-    });
 
 
  app.listen(PORT, ()=>{
---
+++ src/routes/cartsRouter.js
@@ -0,0 +1,62 @@
+import express from "express";
+import CartManager from "../CartManager.js";
+
+const cartManager = new CartManager;
+
+const router = express.Router();
+
+router.get("/api/carts", (req, res)=>{
+
+    res.json(this.carts);
+})
+
+ router.post("/cart", async (req, res)=>{
+    try {
+        const cart=req.body
+        const newCart = await cartManager.addCart(cart)
+        res.json(newCart);
+
+    } catch (error) {
+        console.error("error")
+
+    }
+ });
+
+ router.get("/cart/:cid", async (req, res)=>{
+    try {
+        const id = parseInt (req.params.cid);
+        const cart = await cartManager.getCartById(id);
+        res.json(cart);
+
+
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+
+    router.post("/cart/:cid/product/:pid", async(req, res)=>{
+    try{
+    const cartId=parseInt(req.params.cid);
+    const productId=parseInt(req.params.pid);
+    const cart = await cartManager.addProduct(cartId, productId);
+    res.json(cart)
+    }catch(error){
+     return"error"
+     };
+    });
+
+router.delete("/cart/:cid/product/:pid", async(req, res)=>{
+    try {
+        const cartId = parseInt(req.params.cid);
+        const productId=parseInt(req.params.pid);
+        const cart= await cartManager.deleteProduct(cartId, productId);
+        res.json(cart);
+    } catch (error) {
+        res.status(500).json({error:error.message})
+
+    };
+});
+
+}
+)
+
+export default router 
